8fc674416659dfdc9354bf782873a0f9a9bc28aa
Arjun Singh <arjun7777singh.as@gmail.com>
2024-07-03 23:20:26 -0600

    first commit

---
+++ LeetcodeCSharp/LeetcodeCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetcodeCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public ListNode MergeNodes(ListNode head)
+        {
+            ListNode nextSum = head.next;
+            ListNode modify = nextSum;
+
+            while (nextSum != null)
+            {
+                int sum = 0;
+                while (nextSum.val != 0)
+                {
+                    sum += nextSum.val;
+                    nextSum = nextSum.next;
+                }
+                modify.val = sum;
+                nextSum = nextSum.next;
+                modify.next = nextSum;
+                modify = modify.next;
+            }
+            return head.next;
+
+        }
+
+    }
+    
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+   }
+}
